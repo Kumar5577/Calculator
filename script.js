27eef441d53de47b65812cc6119b4821fdbfc992
RAMYA5577 <mutyamramya0101@gmail.com>
2023-11-06 09:38:16 +0530

    Done

---
+++ script.js
@@ -0,0 +1,26 @@
+let btns = document.querySelectorAll("input");
+
+let string = "";
+
+Array.from(btns).forEach((btn)=>{
+    btn.addEventListener('click',(e)=>{
+        if(e.target.value=='='){
+            string = eval(string);
+            document.querySelector(".text").value = string;
+        }
+        else if(e.target.value=='AC'){
+            string = "";
+            document.querySelector(".text").value = string;
+
+        }
+        else if(e.target.value=='DE'){
+            string = string.slice(0,-1);
+            document.querySelector(".text").value = string;
+        }
+        else{
+        console.log(e.target);
+        string = string + e.target.value;
+        document.querySelector(".text").value = string;
+        }
+    })
+})
